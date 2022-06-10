06507e3f6f7543e57bf334f664659440535acc4a
Sync <103685018+Sync1350@users.noreply.github.com>
2022-06-10 20:59:19 +0100

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo installing dependancies
-pip install -r requirements.txt
-echo Go to /Data/webhook.txt and edit WEBHOOK: "and put your webhook here"
-echo When your done press enter
-pause
-open RobloxGen.exe

c4fc2d52002a45301a789a40e498f6ab5c6dd614
Sync <103685018+Sync1350@users.noreply.github.com>
2022-06-10 20:32:32 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo installing dependancies
+pip install -r requirements.txt
+echo Go to /Data/webhook.txt and edit WEBHOOK: "and put your webhook here"
+echo When your done press enter
+pause
+open RobloxGen.exe
